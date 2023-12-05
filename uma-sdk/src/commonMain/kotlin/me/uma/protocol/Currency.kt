4eb42c6a4069146e44e6ec8b7df3e41955f98e1b
Jeremy Klein <jklein24@gmail.com>
2023-12-05 12:25:09 -0800

    Rename displayDecimals -> decimals to better match a LUD-21 proposal.

--- uma-sdk/src/commonMain/kotlin/me/uma/protocol/Currency.kt
+++ uma-sdk/src/commonMain/kotlin/me/uma/protocol/Currency.kt
@@ -42,12 +42,12 @@ data class Currency @JvmOverloads constructor(
 
     /**
      * Number of digits after the decimal point for display on the sender side. For example,
-     * in USD, by convention, there are 2 digits for cents - $5.95. in this case, `displayDecimals`
+     * in USD, by convention, there are 2 digits for cents - $5.95. in this case, `decimals`
      * would be 2. Note that the multiplier is still always in the smallest unit (cents). This field
      * is only for display purposes. The sender should assume zero if this field is omitted, unless
      * they know the proper display format of the target currency.
      */
-    val displayDecimals: Int? = null,
+    val decimals: Int? = null,
 ) {
     fun toJson() = Json.encodeToString(this)
 }
