696f64d36a401306e0e0527f574f43339bfe7d3c
Jeremy Klein <jklein24@gmail.com>
2024-01-18 22:10:20 -0800

    Use http for more localhost formats

---
+++ uma-sdk/src/commonMain/kotlin/me/uma/utils/Urls.kt
@@ -0,0 +1,10 @@
+package me.uma.utils
+
+fun isDomainLocalhost(domain: String): Boolean {
+    val domainWithoutPort = domain.split(":")[0]
+    val tld = domainWithoutPort.split(".").last()
+    return domainWithoutPort == "localhost" ||
+        tld == "local" ||
+        tld == "internal" ||
+        domainWithoutPort == "127.0.0.1"
+}
